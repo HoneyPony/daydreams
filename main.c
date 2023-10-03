0aa118413cbbff1bb1d933492b9dff9172c6b09c
HoneyPony <honeyponygames@gmail.com>
2023-10-03 19:41:13 -0400

    Experiment with paper texture

--- main.c
+++ main.c
@@ -15,8 +15,16 @@ impl_begin {
 	float clear[4] = { 0.6, 0.6, 0.6, 1.0 };
 	memcpy(clear_color, clear, sizeof(clear));
 
+	Sprite *paper = new(Sprite);
+	paper->z_index = -5;
+	set_lscale(paper, vxy(0.5, 0.5));
+	sprite_play(paper, &res.oga.paper_texture_tex.statc);
+	reparent(paper, root);
+
 	player = new(Player);
 	reparent(player, root);
+
+	
 }
 
 impl_tick_start {
